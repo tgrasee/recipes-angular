1dffbd4c7c42558299ee6f86ccccfc9257e4d05a
tgrasee <tgrasee@gmail.com>
2022-10-18 17:31:25 -0400

    add private recipes array, import recipe, and getRecipes method to return recipe array

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,3 +1,14 @@
+import { Recipe } from "./recipe.model";
+
 export class RecipeService {
-  
+  private recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is simply a test', 
+    'https://upload.wikimedia.org/wikipedia/commons/5/57/990402-ians-recipe-01-IMG_4724.jpg'),
+    new Recipe('Another Test Recipe', 'This is simply a test', 
+    'https://upload.wikimedia.org/wikipedia/commons/5/57/990402-ians-recipe-01-IMG_4724.jpg')
+  ];
+
+  getRecipes() {
+    return this.recipes.slice();
+  }
 }
