dbc7d19913f4ee2343fdf5ccc8f0d2dfa9014ae8
tgrasee <tgrasee@gmail.com>
2022-10-24 16:28:03 -0400

    add ingredientsChanged event

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,6 +1,8 @@
+import { EventEmitter } from "@angular/core";
 import { Ingredient } from "../shared/indredient.model";
 
 export class ShoppingListService {
+  ingredientsChanged = new EventEmitter<Ingredient[]>();
   private ingredients: Ingredient[] = [
     new Ingredient('Apples', 5),
     new Ingredient('Tomatoes', 10),
@@ -13,5 +15,6 @@ export class ShoppingListService {
 
   addIngredient(ingredient: Ingredient) {
     this.ingredients.push(ingredient);
+    this.ingredientsChanged.emit(this.ingredients.slice());
   }
 }
