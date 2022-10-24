cd512b8546cf55217da4c8ec9241076ee4c64933
tgrasee <tgrasee@gmail.com>
2022-10-24 16:41:17 -0400

    add new Ingredient to test recipes

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,14 +1,25 @@
 import { EventEmitter } from "@angular/core";
+import { Ingredient } from "../shared/indredient.model";
 import { Recipe } from "./recipe.model";
 
 export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
 
   private recipes: Recipe[] = [
-    new Recipe('A Test Recipe', 'This is simply a test', 
-    'https://upload.wikimedia.org/wikipedia/commons/5/57/990402-ians-recipe-01-IMG_4724.jpg'),
-    new Recipe('Another Test Recipe', 'This is simply a test', 
-    'https://upload.wikimedia.org/wikipedia/commons/5/57/990402-ians-recipe-01-IMG_4724.jpg')
+    new Recipe('A Test Recipe', 
+    'This is simply a test', 
+    'https://upload.wikimedia.org/wikipedia/commons/5/57/990402-ians-recipe-01-IMG_4724.jpg',
+    [
+      new Ingredient('Meat', 1),
+      new Ingredient('French Fries', 20)
+    ]),
+    new Recipe('Another Test Recipe', 
+    'This is simply a test', 
+    'https://upload.wikimedia.org/wikipedia/commons/5/57/990402-ians-recipe-01-IMG_4724.jpg',
+    [
+      new Ingredient('Bun', 2),
+      new Ingredient('Meat', 1)
+    ])
   ];
 
   getRecipes() {
